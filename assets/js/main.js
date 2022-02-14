2b320003ecb2a16b3ee908c981145549bdaa8ca5
AlexisLeDinahet <alexisledinahet@hotmail.com>
2022-02-14 11:01:15 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+const startDate = new Date("june 1, 2022 00:00:00").getTime();
+
+function decompte() {
+  let now = new Date().getTime();
+  let gap = startDate - now;
+
+  let second = 1000;
+  let minute = second * 60;
+  let hour = minute * 60;
+  let day = hour * 24;
+
+  let d = Math.floor(gap / day);
+  let h = Math.floor((gap % day) / hour);
+  let m = Math.floor((gap % hour) / minute);
+  let s = Math.floor((gap % minute) / second);
+
+  document.getElementById("day").innerText = d;
+  document.getElementById("hour").innerText = h;
+  document.getElementById("minute").innerText = m;
+  document.getElementById("second").innerText = s;
+}
+
+setInterval(function () {
+  decompte();
+}, 1000);
